a3e47dd3c07dc32ff63b764de5bcce157c11d449
Mateo Fernández <qweasdrty13@gmail.com>
2022-05-04 15:54:21 -0300

    1

---
+++ pokedex/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          Pokedex
+        </p>
+    
+
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
